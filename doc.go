6ed1f1d0c0398a3347e7bf5e4fbb553fd90ec00d
Roger Liang <pinguroger@gmail.com>
2019-02-07 21:27:55 +0000

    update doc

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Go module ansible-vault is developed as a go module which can be used by other go tools
+// Go module ansible-vault is developed to be used by other go tools
 // for data encryption or decryption. It is designed to follow the exact spec of
 // Ansible Vault 1.1 so the data it encrypted can be decrypted by Ansible Vault or vice versa.
 // The spec can be found via
