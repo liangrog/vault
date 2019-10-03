b14798d78aaf98e6598b8bf79a0c465401a57f8c
Roger Liang <pinguroger@gmail.com>
2019-10-03 05:15:23 +0000

    fix global v value causing go routine go pear shape because of sharing value

--- vault.go
+++ vault.go
@@ -4,21 +4,19 @@ import (
 	"github.com/liangrog/vault/avault"
 )
 
-var v = new(avault.Vault)
-
 // It encrypt given byte data using provided password
 // into ansible-vault 1.1 format.
 func Encrypt(data []byte, password string) ([]byte, error) {
-	return v.Encrypt(data, password)
+	return new(avault.Vault).Encrypt(data, password)
 }
 
 // It decrypt given byte data in ansible-vault 1.1
 // format using provided password into plain text.
 func Decrypt(password string, data []byte) ([]byte, error) {
-	return v.Decrypt(password, data)
+	return new(avault.Vault).Decrypt(password, data)
 }
 
 // Check if given data has ansible vault header
 func HasVaultHeader(data []byte) bool {
-	return v.HasVaultHeader(data)
+	return new(avault.Vault).HasVaultHeader(data)
 }
